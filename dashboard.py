6583d8bdfd1f14d40607df121832c5ceb6cbaa81
Franz <franz.schwanninger@gmx.net>
2022-03-22 11:38:27 +0100

    disable sensors

--- dashboard.py
+++ dashboard.py
@@ -150,15 +150,21 @@ try:
         #logging.info(res.stdout)
 
         logging.info("Adding visuals to image...")
+        upper = 80
+        lower = 299
+        left = 70
+        right = 330
         #house frame
-        draw.line((70, 299, 330, 299), fill = 0, width = 3)
-        draw.line((70, 299, 70, 120), fill = 0, width = 3)
-        draw.line((70, 120, 330, 120), fill = 0, width = 3)
-        draw.line((330, 120, 330, 299), fill = 0, width = 3)
+        draw.line((left, lower, right, lower), fill = 0, width = 3)
+        draw.line((left, lower, left, upper), fill = 0, width = 3)
+        draw.line((left, upper, right, upper), fill = 0, width = 3)
+        draw.line((right, upper, right, lower), fill = 0, width = 3)
 
         #roof
-        draw.line((200, 1, 0, 185), fill = 0, width = 3)
-        draw.line((200, 1, 400, 185), fill = 0, width = 3)
+        mid = 200
+        y = mid*upper/(mid-left)
+        draw.line((mid, 1, 0, y), fill = 0, width = 3)
+        draw.line((mid, 1, 400, y), fill = 0, width = 3)
         '''draw.line((70, 50, 20, 100), fill = 0)
         draw.rectangle((20, 50, 70, 100), outline = 0)
         draw.line((165, 50, 165, 100), fill = 0)
